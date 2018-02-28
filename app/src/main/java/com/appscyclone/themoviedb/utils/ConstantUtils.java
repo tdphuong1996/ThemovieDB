1c29f96c158d2eb86acc9a712d3e51c94ff5c159
duyphuong <tdphuong96@gmail.com>
2018-02-28 13:26:27 +0700

    +load more

--- app/src/main/java/com/appscyclone/themoviedb/utils/ConstantUtils.java
+++ app/src/main/java/com/appscyclone/themoviedb/utils/ConstantUtils.java
@@ -9,17 +9,20 @@ public class ConstantUtils {
     public static final String LANGUAGE="language";
     public static final String EN_US="en-US";
     public static final String IMAGE_URL="https://image.tmdb.org/t/p/w500";
-    public static final String ERROR_LOGIN="Invalid username and/or password: You did not provide a valid login.";
+//    public static final String ERROR_LOGIN="Invalid username and/or password: You did not provide a valid login.";
     public static final String ACCOUNT_ID = "accountID";
     public static final String USER_NAME = "username";
     public static final String SESSION_ID = "session_id";
-    public static final String MEDIA_TYPE = "media_type";
+//    public static final String MEDIA_TYPE = "media_type";
     public static final String MOVIE = "movie";
-    public static final String MEDIA_ID = "media_id";
+//    public static final String MEDIA_ID = "media_id";
     public static final String FAVORITE = "favorite";
     public static final String GUEST_SESSION_ID = "guest_session_id";
     public static final String SESSION = "session";
+    public static final String SORT_BY = "sort_by";
+    public static final String CREATED_AT = "created_at.desc";
+    public static final String PASS_WORD = "password";
 
     public static String  ID_PEOPLE="idPeople";
-    public static final String REQUEST_TOKEN="request_token";
+    public static final String REQUEST_TOKEN = "request_token";
 }
